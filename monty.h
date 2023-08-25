0586468c881469aac14cbd8d2f72506d0f32a116
field02 <field02>
2023-08-25 13:33:28 +0300

    Too much errors

--- monty.h
+++ monty.h
@@ -36,10 +36,7 @@ typedef struct instruction_s
 {
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack)i; 
+} instruction_t; 
 
 /**
  * struct stack_val - variable
@@ -57,24 +54,5 @@ struct stack_type
 	int queue;
 } stack_val;
 
-void add_failure(stack_t **stack, unsigned int line_number);
-void div_failure(stack_t **stack, unsigned int line_number);
-void file_failure(void);
-void input_failure(void);
-void parse_bytecode_file(FILE *file_ptr);
-int main(int argc, char **argv);
-void malloc_failure(stack_t **stack);
-void mod_failure(stack_t **stack, unsigned int line_number);
-void mul_failure(stack_t **stack, unsigned int line_number);
-void (*op_func(char *s))(stack_t **, unsigned int);
-void opcode_failure(stack_t **stack, unsigned int line_number);
-void pint_failure(stack_t **stack, unsigned int line_number);
-void pop_failure(stack_t **stack, unsigned int line_number);
-void push_failure(stack_t **stack, unsigned int line_number);
-void free_stack(stack_t **stack);
-void swap_failure(stack_t **stack, unsigned int line_number);
-void zero_failure(stack_t **stack, unsigned int line_number);
-void opcode_pall(stack_t **stack, unsigned int line_number);
-void opcode_push(stack_t **head, unsigned int line_number);
-void push_value(stack_t **head, int value);
+
 #endif
